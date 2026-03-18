ca7cb2c9358e5bcab2889d9fba4b9ea5239e6d0e
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-03-18 13:49:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Module sensors 
+
+Provide a description of the purpose of the module and any relevant information.
+
+## Models
+
+This module provides the following model(s):
+
+- [`my-ns:sensors:mysensor`](my-ns_sensors_mysensor.md) - Provide a brief description of the model
---
+++ main.cpp
@@ -0,0 +1,44 @@
+
+#include "mysensor.hpp"
+
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <viam/sdk/common/exception.hpp>
+#include <viam/sdk/common/instance.hpp>
+#include <viam/sdk/log/logging.hpp>
+#include <viam/sdk/registry/registry.hpp>
+
+
+int main(int argc, char** argv) try {
+
+    // Every Viam C++ SDK program must have one and only one Instance object which is created before
+    // any other SDK objects and stays alive until all of them are destroyed.
+    viam::sdk::Instance inst;
+
+    // Write general log statements using the VIAM_SDK_LOG macro.
+    VIAM_SDK_LOG(info) << "Starting up sensors module";
+
+    viam::sdk::Model model("38d7fa87-0f0f-44db-ab05-11f487e80997", "sensors", "mysensor");
+
+
+    auto mr = std::make_shared<viam::sdk::ModelRegistration>(
+        viam::sdk::API::get<viam::sdk::Sensor>(),
+        model,
+        [](viam::sdk::Dependencies deps, viam::sdk::ResourceConfig cfg) {
+            return std::make_unique<sensors::Mysensor>(deps, cfg);
+        },
+        &sensors::Mysensor::validate);
+
+
+
+    std::vector<std::shared_ptr<viam::sdk::ModelRegistration>> mrs = {mr};
+    auto my_mod = std::make_shared<viam::sdk::ModuleService>(argc, argv, mrs);
+    my_mod->serve();
+
+    return EXIT_SUCCESS;
+} catch (const viam::sdk::Exception& ex) {
+    std::cerr << "main failed with exception: " << ex.what() << "\n";
+    return EXIT_FAILURE;
+}
