cbc8aec2137cbe63bf7d4178324f26b98218f139
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-04-20 12:19:49 -0400

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Module bullseye-test 
+
+Provide a description of the purpose of the module and any relevant information.
+
+## Models
+
+This module provides the following model(s):
+
+- [`my-ns:bullseye-test:bseye`](my-ns_bullseye-test_bseye.md) - Provide a brief description of the model
---
+++ src/bseye.cpp
@@ -0,0 +1,40 @@
+
+#include "bseye.hpp"
+
+#include <stdexcept>
+
+namespace bullseye-test {
+
+
+Bseye::Bseye(const viam::sdk::Dependencies& deps, const viam::sdk::ResourceConfig& cfg)
+    : Sensor(cfg.name()) {
+    throw std::runtime_error("constructor not implemented");
+}
+
+
+std::vector<std::string> Bseye::validate(const viam::sdk::ResourceConfig& cfg) {
+    throw std::runtime_error("\"validate\" not implemented");
+}
+
+viam::sdk::ProtoStruct Bseye::do_command(const viam::sdk::ProtoStruct & command)
+{
+    throw std::logic_error("\"do_command\" not implemented");
+}
+
+viam::sdk::ProtoStruct Bseye::get_status()
+{
+    throw std::logic_error("\"get_status\" not implemented");
+}
+
+std::vector<viam::sdk::GeometryConfig> Bseye::get_geometries(const viam::sdk::ProtoStruct & extra)
+{
+    throw std::logic_error("\"get_geometries\" not implemented");
+}
+
+viam::sdk::ProtoStruct Bseye::get_readings(const viam::sdk::ProtoStruct & extra)
+{
+    throw std::logic_error("\"get_readings\" not implemented");
+}
+
+} // namespace bullseye-test
+
