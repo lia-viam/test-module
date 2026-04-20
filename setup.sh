7fc1aea41b92b63a4c66e8cfbdb9319ef3c52360
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-04-20 15:20:01 -0400

    try cpp20 build

---
+++ setup.sh
@@ -0,0 +1,20 @@
+bash -c 'wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -'
+apt-add-repository -y 'deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-15 main'
+apt-add-repository -y 'deb https://archive.debian.org/debian bullseye-backports main'
+
+apt-get update
+
+apt-get -y --no-install-recommends install -t llvm-toolchain-bullseye-15 \
+    clang-15 \
+
+echo '[settings]
+arch=armv8
+build_type=Release
+compiler=clang
+compiler.cppstd=20
+compiler.libcxx=libstdc++11
+compiler.version=15
+os=Linux' > ~/.conan2/profiles/default
+
+conan profile show
+
