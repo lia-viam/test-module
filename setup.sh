fe14ac9d7dd4b711db0e18a3dd5bfe0bd1696ff2
Lia Stratopoulos <167905060+lia-viam@users.noreply.github.com>
2026-05-18 15:07:25 -0400

    remove clang profile

--- setup.sh
+++ setup.sh
@@ -3,35 +3,6 @@ set -e
 apt-get update
 apt-get install -y software-properties-common
 
-bash -c 'wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -'
-apt-add-repository -y 'deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-19 main'
-apt-add-repository -y 'deb https://archive.debian.org/debian bullseye-backports main'
-
-apt-get update
-
-apt-get -y install -t llvm-toolchain-bullseye-19 \
-    clang-19 libc++-19-dev libc++abi-19-dev
-
-echo '[settings]
-arch={{detect_api.detect_arch()}}
-build_type=Release
-compiler=clang
-compiler.cppstd=17
-compiler.libcxx=libc++
-compiler.version=19
-os=Linux
-
-[conf]
-tools.build:compiler_executables={"c": "clang-19", "cpp": "clang++-19"}
-tools.build:exelinkflags+=["-static-libstdc++"]
-tools.build:sharedlinkflags+=["-static-libstdc++"]
-
-[buildenv]
-LD_LIBRARY_PATH+=/usr/lib/llvm-19/lib
-
-[options]
-' > ~/.conan2/profiles/default
-
 conan profile show
 
 cmake --version
